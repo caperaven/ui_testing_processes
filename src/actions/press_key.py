d0d3f5d5f929d049b34123844fb41e7376c3cf27
6Botter7 <7williamsandre7@gmail.com>
2023-06-08 08:37:55 +0200

    Revert "Added functionality for multiple key-presses"
    This reverts commit 76f2af9bc82227be87af4c3d1615e6a8879d7a5e.

--- src/actions/press_key.py
+++ src/actions/press_key.py
@@ -4,10 +4,7 @@ from src.elements import get_element
 from src.errors import set_error
 from src.utils import get_name
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 
-
-from selenium.webdriver.common.keys import Keys
 
 async def press_key(driver, args, results):
     try:
@@ -16,30 +13,13 @@ async def press_key(driver, args, results):
         if element is None:
             return
 
-        key_values = args["key"].split("+")
-
-        if len(key_values) > 1:
-            action_chains = ActionChains(driver)
-
-            for key_value in key_values:
-                if key_value.upper() == "CONTROL":
-                    action_chains.key_down(Keys.CONTROL)
-                elif key_value.upper() == "SHIFT":
-                    action_chains.key_down(Keys.SHIFT)
-                elif key_value.upper() == "ALT":
-                    action_chains.key_down(Keys.ALT)
-                else:
-                    action_chains.key_down(key_value)
-
-            action_chains.perform()
-        else:
-            element.send_keys(key_values[0])
-
+        key_value = args["key"]
+        key = getattr(Keys, key_value)
+        element.send_keys(key)
     except StaleElementReferenceException:
         time.sleep(0.25)
         await press_key(driver, args, results)
         pass
-
     except Exception as e:
         print(e)
         name = get_name(args)
